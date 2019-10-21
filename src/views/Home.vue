526855d1bfe747f71d8452f83729c3c8fb6d7de8
Kian Mayne <git@kianmayne.co.uk>
2019-10-21 21:55:13 +0100

    Add initial UI

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <ul class="todo-list">
+      <li v-for="item in todoItems">
+        <i class="checkbox material-icons" @click="() => item.done = true">check_box_outline_blank</i>
+        <span contenteditable="true">{{item.text}}</span>
+      </li>
+      <li>
+        <i class="checkbox material-icons">add</i>
+        <span contenteditable="true"></span>
+      </li>
+      <li v-for="item in doneItems">
+        <i class="checkbox material-icons" @click="() => item.done = false">check_box</i>
+        <span class="done" contenteditable="true">{{item.text}}</span>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -12,7 +24,66 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 export default Vue.extend({
   name: 'home',
   components: {
-    HelloWorld,
   },
+  data: () => ({
+    items: [
+      { id: 1, text: "Item 1", done: false },
+      { id: 2, text: "Item 2", done: false },
+      { id: 3, text: "Item 3", done: true }
+    ]
+  }),
+  computed: {
+    todoItems() {
+      return this.items.filter(i => !i.done);
+    },
+    doneItems() {
+      return this.items.filter(i => i.done);
+    }
+  },
+  methods: {
+  }
 });
 </script>
+
+<style>
+.home {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  padding: 8px;
+}
+
+.todo-list {
+  border: 1px solid black;
+  border-radius: 3px;
+  box-sizing: border-box;
+  margin: 0;
+  list-style: none;
+  padding: 0;
+  text-align: left;
+}
+
+.todo-list > li {
+  padding: 4px;
+  display: flex;
+  align-items: center;
+}
+
+.todo-list > li > i {
+  flex: none;
+}
+
+.todo-list > li > span {
+  display: inline-block;
+  flex: auto;
+}
+
+.todo-list span.done {
+  text-decoration: line-through;
+}
+
+.checkbox {
+  vertical-align: text-bottom;
+}
+</style>
